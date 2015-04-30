6f383458b57c00ac4084cbdda36f7f7ae0cb7f0a
likoms <toribosh@gmial.com>
2015-04-30 10:38:19 +0200

    Removed folder from repository

--- Debug/src/Dijkstra.d
+++
@@ -1,6 +0,0 @@
-src/Dijkstra.d: ../src/Dijkstra.cpp ../src/Dijkstra.h \
- ../src/AdjacencyMatrix.h
-
-../src/Dijkstra.h:
-
-../src/AdjacencyMatrix.h:
--- Debug/src/Graph.d
+++
@@ -1,8 +0,0 @@
-src/Graph.d: ../src/Graph.cpp ../src/Dijkstra.h ../src/AdjacencyMatrix.h \
- ../src/Screen.h
-
-../src/Dijkstra.h:
-
-../src/AdjacencyMatrix.h:
-
-../src/Screen.h:
