fcfb46e835a2fd3e0565bd0edf1df54866c8fbd8
likoms <toribosh@gmial.com>
2015-05-07 18:51:27 +0200

    Add new class

---
+++ src/Kruskal.cpp
@@ -0,0 +1,18 @@
+/*
+ * Kruskal.cpp
+ *
+ *  Created on: May 7, 2015
+ *      Author: piotr
+ */
+
+#include "Kruskal.h"
+
+Kruskal::Kruskal() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Kruskal::~Kruskal() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Kruskal.h
@@ -0,0 +1,17 @@
+/*
+ * Kruskal.h
+ *
+ *  Created on: May 7, 2015
+ *      Author: piotr
+ */
+
+#ifndef KRUSKAL_H_
+#define KRUSKAL_H_
+
+class Kruskal {
+public:
+	Kruskal();
+	virtual ~Kruskal();
+};
+
+#endif /* KRUSKAL_H_ */
